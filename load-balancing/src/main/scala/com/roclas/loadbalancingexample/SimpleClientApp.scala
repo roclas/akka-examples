9997504aed0d9f10c53117c87bfa96cbf1f78d6f
Javier Santos <javiersantospaniego@gmail.com>
2015-03-05 15:29:39 +0100

    Refactored code to make it simpler. Besides that, it also runs (there were too many Cluster instances before).

--- load-balancing/src/main/scala/com/roclas/loadbalancingexample/SimpleClientApp.scala
+++ load-balancing/src/main/scala/com/roclas/loadbalancingexample/SimpleClientApp.scala
@@ -4,37 +4,36 @@ import akka.actor._
 import akka.contrib.pattern.ClusterClient
 import com.typesafe.config.ConfigFactory
 
-
 object SimpleClientApp extends App {
 
   // Override the configuration of the port
   // when specified as program argument
-  //if (args.nonEmpty) System.setProperty("akka.remote.netty.tcp.port", args(0))
+  if (args.nonEmpty)
+    System.setProperty("akka.remote.netty.tcp.port", args(0))
 
   // Create an Akka system
-  implicit val system = ActorSystem("ClientSystem", ConfigFactory.load.getConfig("clusterclient"))
-  //implicit val system = ActorSystem("ClusterSystem", ConfigFactory.load.getConfig("clusterclient"))
+  implicit val system = ActorSystem(
+    "ClientSystem",
+    ConfigFactory.load.getConfig("clusterclient"))
 
-  val initialContacts = Set(
-    //system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2551/user/receptionist"),
-    system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2551/user/receptionist"))
+  val contacts = List(
+    "akka.tcp://ClusterSystem@127.0.0.1:2551/user/receptionist")
+
+  val initialContacts = contacts.map(system.actorSelection).toSet
 
   println("Starting Cluster Client")
-  val c = system.actorOf(ClusterClient.props(initialContacts))
-  val receptionistActor0=system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2551/")
-  val receptionistActor=system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2551/user/receptionist")
-  val serviceActor=system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2551/user/service")
 
-  while(true){
-      Thread.sleep(5000)
-      serviceActor ! "hola serviceActor"
-      receptionistActor0 ! "hola receptionistActor0"
-      receptionistActor ! "hola receptionistActor"
-      println("Sending message from Cluster Client")
-      c ! ClusterClient.Send("/user/receptionist", "hello", localAffinity = true)
-      Thread.sleep(2000)
-      println("Sending message from Cluster Client to /user/service")
-      c ! ClusterClient.Send("/user/service", "hello", localAffinity = true)
-  }
+  val client = system.actorOf(ClusterClient.props(initialContacts))
+
+  val stream: Stream[Int] = 0 #:: stream.map(_+1)
+  stream.foreach(i => {
+    println(s"Sending message $i")
+    client ! ClusterClient.Send(
+      "/user/workingService",
+      s"This is request $i",
+      localAffinity = true)
+    Thread.sleep(1000)
+  })
+
 
 }
--- load-balancing/src/main/scala/com/roclas/loadbalancingexample/SimpleClusterApp.scala
+++ load-balancing/src/main/scala/com/roclas/loadbalancingexample/SimpleClusterApp.scala
@@ -6,28 +6,28 @@ import akka.cluster.ClusterEvent._
 import akka.contrib.pattern.ClusterReceptionistExtension
 import com.typesafe.config.ConfigFactory
 
-
-
 object SimpleClusterApp extends App {
 
-  // Override the configuration of the port
-  // when specified as program argument
-  if (args.nonEmpty){
-    System.setProperty("clusterserver.akka.remote.netty.tcp.port", args(0))
-    println(s"\n\n\n\n\nclusterserver.akka.remote.netty.tcp.port=${args(0)}\n\n\n\n\n")
-  }else{
-    val port=System.getProperty("clusterserver.akka.remote.netty.tcp.port")
-    println(s"\n\n\n\n\nclusterserver.akka.remote.netty.tcp.port=${port}\n\n\n\n\n")
-  }
-
   // Create an Akka system
-  implicit val system = ActorSystem("ClusterSystem", ConfigFactory.load.getConfig("clusterserver"))
+  implicit val system = ActorSystem(
+    "ClusterSystem",
+    ConfigFactory.load.getConfig("clusterserver"))
 
-  val clusterListener = system.actorOf(Props( new SimpleClusterListener()), name = "clusterListener")
-  Cluster(system).subscribe(clusterListener, classOf[ClusterDomainEvent])
+  val cluster: Cluster =
+    Cluster(system)
 
-  val service = system.actorOf(Props(new WorkingActor()), "workingService")
-  ClusterReceptionistExtension(system).registerService(service)
+  val receptionistExtension: ClusterReceptionistExtension =
+    ClusterReceptionistExtension(system)
+
+  val clusterListener = system.actorOf(Props(
+    new SimpleClusterListener(cluster)),
+    "clusterListener")
+
+  val service = system.actorOf(Props(
+    new WorkingActor()),
+    "workingService")
+
+  receptionistExtension.registerService(service)
 
 
 }
